14927b44eb8f2462c69894aaca2b08161521757e
Pete Dring <dringp@fulford.york.sch.uk>
2023-06-07 20:52:18 +0100

    Add project files.

---
+++ MBFlasher.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace MicroBitAutoFlasher
+{
+    partial class MBFlasher
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MBFlasher));
+            statusStrip = new StatusStrip();
+            statusLabel = new ToolStripStatusLabel();
+            timer = new System.Windows.Forms.Timer(components);
+            lstLog = new ListBox();
+            statusStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            statusStrip.Items.AddRange(new ToolStripItem[] { statusLabel });
+            statusStrip.Location = new Point(0, 428);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(800, 22);
+            statusStrip.TabIndex = 0;
+            statusStrip.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(57, 17);
+            statusLabel.Text = "Welcome";
+            // 
+            // timer
+            // 
+            timer.Enabled = true;
+            timer.Interval = 2000;
+            timer.Tick += timer_Tick;
+            // 
+            // lstLog
+            // 
+            lstLog.Dock = DockStyle.Fill;
+            lstLog.FormattingEnabled = true;
+            lstLog.ItemHeight = 15;
+            lstLog.Location = new Point(0, 0);
+            lstLog.Name = "lstLog";
+            lstLog.Size = new Size(800, 428);
+            lstLog.TabIndex = 1;
+            // 
+            // MBFlasher
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstLog);
+            Controls.Add(statusStrip);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MBFlasher";
+            Text = "Micro:bit auto flasher tool";
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private StatusStrip statusStrip;
+        private ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.Timer timer;
+        private ListBox lstLog;
+    }
+}
---
+++ MBFlasher.cs
@@ -0,0 +1,121 @@
+using RoboTester;
+using System.Text;
+
+namespace MicroBitAutoFlasher
+{
+    public partial class MBFlasher : Form
+    {
+        string downloadsPath;
+        bool mbDetected = false;
+        FileSystemWatcher watcher;
+        string mbPath;
+        Dictionary<string, bool> ignoreFileList;
+
+        public MBFlasher()
+        {
+            InitializeComponent();
+            downloadsPath = KnownFolders.GetPath(KnownFolder.Downloads);
+            watcher = new FileSystemWatcher();
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Changed += OnNewFileDetected;
+
+            CodePagesEncodingProvider.Instance.GetEncoding(850);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            lstLog.Items.Add("Leave this program running while you use an online code editor for the micro:bit");
+            lstLog.Items.Add("This program will scan your downloads folder for any .hex files that you save.");
+            lstLog.Items.Add("If a micro:bit is plugged in it will attempt to copy it automatically");
+
+            StartWatchingFiles(downloadsPath, "*.hex");
+        }
+
+        private bool DetectMicrobit()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            for (int i = 0; i < drives.Count(); i++)
+            {
+                if (drives[i].VolumeLabel == "MICROBIT")
+                {
+                    mbPath = drives[i].Name;
+                    mbDetected = true;
+                    return mbDetected;
+                }
+            }
+            return false;
+
+        }
+
+        private void log(string message)
+        {
+            if (message != null)
+            {
+                lstLog.Invoke(() =>
+                {
+                    lstLog.Items.Add(message);
+                    lstLog.TopIndex = lstLog.Items.Count - 1;
+                });
+            }
+        }
+
+        private void OnNewFileDetected(object source, FileSystemEventArgs e)
+        {
+            // only start processing file once, when it's been detected once already. Ignore all other times this file is detected
+            if (ignoreFileList.ContainsKey(e.Name))
+            {
+                log($"Processing file {e.Name}");
+            }
+            else
+            {
+                ignoreFileList.Add(e.Name, true);
+                log($"New hex file detected - waiting for file to be fully saved... {e.Name}");
+                return;
+            }
+
+            // Only try to copy the file if a micro:bit has been detected
+            if (!mbDetected)
+            {
+                log($"Ignoring because micro:bit not detected - is it plugged in?");
+                return;
+            }
+
+            // copy the file
+            RobustCopy.CopyFile(downloadsPath, e.Name, mbPath, (string msg) =>
+            {
+                log(msg);
+                return 0;
+            });
+            log($"Done copying {e.Name} to {mbPath}");
+
+
+        }
+
+        private void StartWatchingFiles(string path, string filter)
+        {
+            watcher.Path = path;
+            watcher.Filter = filter;
+            watcher.EnableRaisingEvents = true;
+            ignoreFileList = new Dictionary<string, bool>();
+        }
+
+        private void StopWatchingFiles()
+        {
+            watcher.EnableRaisingEvents = false;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            string status = "";
+            if (DetectMicrobit())
+            {
+                status = $"Microbit detected: {mbPath}";
+            }
+            else
+            {
+                status = "Microbit not detected";
+            }
+            status += watcher.EnableRaisingEvents ? $" watching {downloadsPath} for new .hex files" : " not watching for new hex files";
+            statusLabel.Text = status;
+        }
+
+
+    }
+}
